1994feb999a9cbb28230397a6a95a74d0f518df5
reshm <reshm@DESKTOP-F10JV1T>
2024-09-13 20:32:26 +0530

    Test Class-Added playMoves Method:to reduce redundancy;added constants

--- tictactoekata/src/test/java/com/tdd/tictactoekata/service/TicTacToeServiceTest.java
+++ tictactoekata/src/test/java/com/tdd/tictactoekata/service/TicTacToeServiceTest.java
@@ -1,14 +1,18 @@
 package com.tdd.tictactoekata.service;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.stream.IntStream;
-
 import org.junit.jupiter.api.Test;
-
 import com.tdd.tictactoekata.model.PlayerMove;
 
 public class TicTacToeServiceTest {
+
+	public static final String GAME_OVER = "Game is already over";
+	public static final String INVALID_MOVE = "Invalid move";
+	public static final String GAME_RESET = "Game Reset";
+	public static final String WINNER_X = "Player X wins!";
+	public static final String WINNER_O = "Player O wins!";
+	public static final String DRAW = "The game is a draw!";
+	public static final String MOVE_COMPELETED = "Move completed";
 
 	TicTacToeService ticTacToeService = new TicTacToeService();
 
@@ -16,76 +20,68 @@ public class TicTacToeServiceTest {
 	public void testMakeMove_PlayerMakesMove_ReturnMoveCompleted() {
 
 		PlayerMove player = new PlayerMove(0, 0, 'X');
-
 		String result = ticTacToeService.makeMove(player);
-
-		assertTrue(result.contains("Move completed"));
+		assertTrue(result.contains(MOVE_COMPELETED));
 	}
 
 	@Test
 	public void testMakeMove_PlayerMakesInvalidMove_ReturnInvalidMove() {
 
 		PlayerMove player = new PlayerMove(3, 0, 'X');
-
 		String result = ticTacToeService.makeMove(player);
-
-		assertTrue(result.contains("Invalid move"));
+		assertTrue(result.contains(INVALID_MOVE));
 	}
 
 	@Test
 	public void testMakeMove_CheckBoardIsFull_ReturnGameIsDraw() {
 
 		String result = fillBoard();
-
-		assertTrue(result.contains("The game is a draw!"));
+		assertTrue(result.contains(DRAW));
 	}
 
 	@Test
 	public void testMakeMove_PlayerWinsByCompletingARow_ReturnPlayerWhoWins() {
 
-		ticTacToeService.makeMove(new PlayerMove(0, 0, 'X'));
-		ticTacToeService.makeMove(new PlayerMove(0, 1, 'X'));
+		playMoves(new PlayerMove(0, 0, 'X'), new PlayerMove(0, 1, 'X'));
 		String result = ticTacToeService.makeMove(new PlayerMove(0, 2, 'X'));
-
-		assertTrue(result.contains("Player X wins!"));
+		assertTrue(result.contains(WINNER_X));
 	}
 
 	@Test
 	public void testMakeMove_PlayerWinsByCompletingAColumn_ReturnPlayerWhoWins() {
 
-		ticTacToeService.makeMove(new PlayerMove(0, 0, 'O'));
-		ticTacToeService.makeMove(new PlayerMove(1, 0, 'O'));
+		playMoves(new PlayerMove(0, 0, 'O'), new PlayerMove(1, 0, 'O'));
 		String result = ticTacToeService.makeMove(new PlayerMove(2, 0, 'O'));
-
-		assertTrue(result.contains("Player O wins!"));
+		assertTrue(result.contains(WINNER_O));
 	}
 
 	@Test
 	public void testMakeMove_PlayerWinsByCompletingADiagonal_ReturnPlayerWhoWins() {
 
-		ticTacToeService.makeMove(new PlayerMove(0, 0, 'O'));
-		ticTacToeService.makeMove(new PlayerMove(1, 1, 'O'));
+		playMoves(new PlayerMove(0, 0, 'O'), new PlayerMove(1, 1, 'O'));
 		String result = ticTacToeService.makeMove(new PlayerMove(2, 2, 'O'));
-
-		assertTrue(result.contains("Player O wins!"));
+		assertTrue(result.contains(WINNER_O));
 	}
 
 	@Test
 	public void testMakeMove_CheckTheGameIsWonOrDraw_ReturnGameOver() {
 
-		ticTacToeService.makeMove(new PlayerMove(0, 0, 'O'));
-		ticTacToeService.makeMove(new PlayerMove(1, 1, 'O'));
-		ticTacToeService.makeMove(new PlayerMove(2, 2, 'O'));
+		playMoves(new PlayerMove(0, 0, 'O'), new PlayerMove(1, 1, 'O'), new PlayerMove(2, 2, 'O'));
 		String result = ticTacToeService.makeMove(new PlayerMove(1, 0, 'X'));
+		assertTrue(result.contains(GAME_OVER));
+	}
 
-		assertTrue(result.contains("Game is already over"));
+	@Test
+	public void testReset_ResetTheGame_ReturnGameReset() {
+
+		String result = ticTacToeService.resetGame();
+		assertTrue(result.contains(GAME_RESET));
 	}
 
 	private String fillBoard() {
 
 		String result = "";
 		char[][] moves = { { 'X', 'O', 'X' }, { 'O', 'O', 'X' }, { 'X', 'X', 'O' } };
-
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
 				result = ticTacToeService.makeMove(new PlayerMove(i, j, moves[i][j]));
@@ -93,12 +89,10 @@ public class TicTacToeServiceTest {
 		}
 		return result;
 	}
-	
-	@Test
-	public void testReset_ResetTheGame_ReturnGameReset() {
 
-		String result = ticTacToeService.resetGame();
-
-		assertTrue(result.contains("Game Reset!"));
+	private void playMoves(PlayerMove... moves) {
+		for (PlayerMove move : moves) {
+			ticTacToeService.makeMove(move);
+		}
 	}
 }
